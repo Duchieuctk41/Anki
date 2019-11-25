206b92bbd0476cef48b66dd807cbd1a3c7014642
Duc Hieu <duchieu.ctk41@gmail.com>
2019-11-25 17:11:45 +0700

    a

--- AAA/app/src/main/java/admin/example/aaa/MainActivity.java
+++ AAA/app/src/main/java/admin/example/aaa/MainActivity.java
@@ -3,12 +3,16 @@ package admin.example.aaa;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     ProgressBar progressBar;
     TextView textView;
+
     int[] hinh ={
             R.drawable.apple,R.drawable.avocado,R.drawable.banana,
             R.drawable.coconut, R.drawable.grape,R.drawable.papaya,
@@ -50,12 +55,17 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(question);
 
 
-                CountDownTimer countDownTimer = new CountDownTimer(20000,200) {
+                CountDownTimer countDownTimer = new CountDownTimer(110,100) {
                     @Override
                     public void onTick(long millisUntilFinished) {
                         int current = progressBar.getProgress();
 
-                        progressBar.setProgress(current - 2);
+                        progressBar.setProgress(current - 200);
+                        if(current<=0)
+                        {
+                            Dialog();
+
+                        }
 
                     }
 
@@ -76,6 +86,19 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);
         progressBar = (ProgressBar) findViewById(R.id.progressbar);
         textView = (TextView) findViewById(R.id.textView);
-
+    }
+    private void Dialog(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_dialog);
+        final ImageView animation = (ImageView) dialog.findViewById(R.id.animation);
+        final Animation animRotate = AnimationUtils.loadAnimation(this,R.anim.anim_);
+        /*animation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animRotate);
+            }
+        });*/
+        animation.startAnimation(animRotate);
+        dialog.show();
     }
 }
